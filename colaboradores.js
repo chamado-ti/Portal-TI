5a6519b360fe819049e70a10a4437ef10168bd55
chamado-ti <ti.aprendiz.esquadromil@gmail.com>
2025-09-17 16:59:33 -0300

    Update colaboradores.js

--- colaboradores.js
+++ colaboradores.js
@@ -1,25 +1,29 @@
 const colaboradores = [
-  { 
-    nome: "Matheus Nunes", 
-    setor: "TI", 
-    hardware: "INSPIRON 15 3565 RAM 8GB SSD 240GB", 
-    foto: "imagens/skype.jpeg" || "imagens/fallback.png", // fallback caso a imagem não exista
+  {
+    nome: "Matheus Nunes",
+    setor: "TI",
+    hardware: "INSPIRON 15 3565 RAM 8GB SSD 240GB",
+    foto: "imagens/skype.jpeg",
     teams: "",
-    chamados: { 
+    chamados: {
       "2025": { Janeiro:0,Fevereiro:0,Março:0,Abril:0,Maio:0,Junho:0,Julho:0,Agosto:0,Setembro:0,Outubro:0,Novembro:0,Dezembro:0 },
-      "2026": { Janeiro:0,Fevereiro:0,Março:0,Abril:0,Maio:0,Junho:0,Julho:0,Agosto:0,Setembro:0,Outubro:0,Novembro:0,Dezembro:0 }
+      "2026": { Janeiro:0,Fevereiro:0,Março:0,Abril:0,Maio:0,Junho:0,Julho:0,Agosto:0,Setembro:0,Outubro:0,Novembro:0,Dezembro:0 },
+      "2027": { Janeiro:0,Fevereiro:0,Março:0,Abril:0,Maio:0,Junho:0,Julho:0,Agosto:0,Setembro:0,Outubro:0,Novembro:0,Dezembro:0 },
+      "2028": { Janeiro:0,Fevereiro:0,Março:0,Abril:0,Maio:0,Junho:0,Julho:0,Agosto:0,Setembro:0,Outubro:0,Novembro:0,Dezembro:0 }
     }
   },
-  { 
-    nome: "Teste", 
-    setor: "Financeiro", 
-    hardware: "", 
-    foto: "imagens/fallback.png", 
-    teams: "", 
-    chamados: { 
+  {
+    nome: "Teste",
+    setor: "Financeiro",
+    hardware: "",
+    foto: "",
+    teams: "",
+    chamados: {
       "2025": { Janeiro:0,Fevereiro:0,Março:0,Abril:0,Maio:0,Junho:0,Julho:0,Agosto:0,Setembro:0,Outubro:0,Novembro:0,Dezembro:0 },
-      "2026": { Janeiro:0,Fevereiro:0,Março:0,Abril:0,Maio:0,Junho:0,Julho:0,Agosto:0,Setembro:0,Outubro:0,Novembro:0,Dezembro:0 }
+      "2026": { Janeiro:0,Fevereiro:0,Março:0,Abril:0,Maio:0,Junho:0,Julho:0,Agosto:0,Setembro:0,Outubro:0,Novembro:0,Dezembro:0 },
+      "2027": { Janeiro:0,Fevereiro:0,Março:0,Abril:0,Maio:0,Junho:0,Julho:0,Agosto:0,Setembro:0,Outubro:0,Novembro:0,Dezembro:0 },
+      "2028": { Janeiro:0,Fevereiro:0,Março:0,Abril:0,Maio:0,Junho:0,Julho:0,Agosto:0,Setembro:0,Outubro:0,Novembro:0,Dezembro:0 }
     }
   },
-  // outros colaboradores...
+  // Adicione os outros colaboradores seguindo o mesmo padrão até 30
 ];
