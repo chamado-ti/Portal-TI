677e06f119f5d299a05701ed7d0f7da785d93e06
chamado-ti <ti.aprendiz.esquadromil@gmail.com>
2025-09-18 13:30:14 -0300

    Update colaboradores.js

--- colaboradores.js
+++ colaboradores.js
@@ -3,32 +3,8 @@ const colaboradores = [
     nome: "Matheus Nunes", 
     setor: "TI", 
     hardware: "Notebook Dell", 
-    foto: "logo.esquadromil.branca.png", 
-    teams: "https://teams.microsoft.com/l/chat/0/0?users=email@empresa.com", 
-    chamados: { 
-      2023: { Janeiro: 1, Fevereiro: 9, Março: 3, Abril: 0, Maio: 0, Junho: 0, Julho: 0, Agosto: 0, Setembro: 0, Outubro: 0, Novembro: 0, Dezembro: 0 },
-      2024: { Janeiro: 0, Fevereiro: 0, Março: 0, Abril: 0, Maio: 0, Junho: 0, Julho: 0, Agosto: 0, Setembro: 0, Outubro: 0, Novembro: 0, Dezembro: 0 },
-      2025: { Janeiro: 2, Fevereiro: 5, Março: 0, Abril: 0, Maio: 0, Junho: 0, Julho: 0, Agosto: 0, Setembro: 0, Outubro: 0, Novembro: 0, Dezembro: 0 },
-      2026: { Janeiro: 0, Fevereiro: 0, Março: 0, Abril: 0, Maio: 0, Junho: 0, Julho: 0, Agosto: 0, Setembro: 0, Outubro: 0, Novembro: 0, Dezembro: 0 },
-      2027: { Janeiro: 0, Fevereiro: 0, Março: 0, Abril: 0, Maio: 0, Junho: 0, Julho: 0, Agosto: 0, Setembro: 0, Outubro: 0, Novembro: 0, Dezembro: 0 }
-    }
+    foto: "img/logo.esquadromil.branca.png", 
+    teams: "https://teams.microsoft.com/l/chat/0/0?users=matheus@empresa.com", 
+    chamados: { Janeiro: 2, Fevereiro: 5 } 
   },
-  // só adicionar novos colaboradores copiando esse objeto 👇
-  /*
-  { 
-    nome: "Fulano da Silva", 
-    setor: "RH", 
-    hardware: "Desktop Lenovo", 
-    foto: "imagens/fulano.png", 
-    teams: "https://teams.microsoft.com/l/chat/0/0?users=fulano@empresa.com", 
-    chamados: { 
-      2023: { Janeiro: 0, Fevereiro: 0, Março: 0, Abril: 0, Maio: 0, Junho: 0, Julho: 0, Agosto: 0, Setembro: 0, Outubro: 0, Novembro: 0, Dezembro: 0 },
-      2024: { Janeiro: 0, Fevereiro: 0, Março: 0, Abril: 0, Maio: 0, Junho: 0, Julho: 0, Agosto: 0, Setembro: 0, Outubro: 0, Novembro: 0, Dezembro: 0 },
-      2025: { Janeiro: 0, Fevereiro: 0, Março: 0, Abril: 0, Maio: 0, Junho: 0, Julho: 0, Agosto: 0, Setembro: 0, Outubro: 0, Novembro: 0, Dezembro: 0 },
-      2026: { Janeiro: 0, Fevereiro: 0, Março: 0, Abril: 0, Maio: 0, Junho: 0, Julho: 0, Agosto: 0, Setembro: 0, Outubro: 0, Novembro: 0, Dezembro: 0 },
-      2027: { Janeiro: 0, Fevereiro: 0, Março: 0, Abril: 0, Maio: 0, Junho: 0, Julho: 0, Agosto: 0, Setembro: 0, Outubro: 0, Novembro: 0, Dezembro: 0 }
-    }
-  }
-  */
 ];
-
