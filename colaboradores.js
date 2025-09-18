40ad503c621fbfdc46ee6977438c0075c5d8d602
chamado-ti <ti.aprendiz.esquadromil@gmail.com>
2025-09-18 13:40:02 -0300

    Update colaboradores.js

--- colaboradores.js
+++ colaboradores.js
@@ -3,8 +3,9 @@ const colaboradores = [
     nome: "Matheus Nunes", 
     setor: "TI", 
     hardware: "Notebook Dell", 
-    foto: "img/logo.esquadromil.branca.png", 
+    foto: "img/imagem (17).png", 
     teams: "https://teams.microsoft.com/l/chat/0/0?users=matheus@empresa.com", 
     chamados: { Janeiro: 2, Fevereiro: 5 } 
   },
 ];
+
